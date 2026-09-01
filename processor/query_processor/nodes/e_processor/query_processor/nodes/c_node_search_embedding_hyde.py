999abe051ad81ae991cb91f536b6140ed62c6470
zc8163623 <2310142249@qq.com>
2026-09-02 01:02:43 +0800

    111

--- processor/query_processor/nodes/e_processor/query_processor/nodes/c_node_search_embedding_hyde.py
+++ processor/query_processor/nodes/e_processor/query_processor/nodes/c_node_search_embedding_hyde.py
@@ -1,6 +1,13 @@
+import json
+
+from config.milvus_config import milvus_config
 from processor.query_processor.base import NodeBase
+from processor.query_processor.nodes.e_processor.query_processor.prompt.search_embedding_hyde import HYDE_PROMPT
 from processor.query_processor.state import QueryGraphState
 from tool.logger import logger
+from utils.embedding_utils import generate_embeddings
+from utils.llm_utils import get_llm_client
+from utils.milvus_utils import get_milvus_client, create_hybrid_search_requests, hybrid_search
 
 
 class NodeSearchEmbeddingHyde(NodeBase):
@@ -22,5 +29,80 @@ class NodeSearchEmbeddingHyde(NodeBase):
         # TODO
         logger.info(f"【{self.name}】节点逻辑")
 
+        # 1 参数处理
+        item_names = state.get("item_names")
+        rewritten_query = state.get("rewritten_query")
+
+        # 2 大模型生成假设性文档
+        hyde_doc = self._step_1_create_embedding_hyde_doc(rewritten_query)
+        print(hyde_doc)
+
+        # 3 用"重写问题+假设性文档"搜索文本块
+        response = self._step_2_search_embedding_hyde(
+            rewritten_query=rewritten_query,
+            hyde_doc=hyde_doc,
+            item_names=item_names,
+        )
+
+        # 4 结果解析
+        # json_response = json.dumps(response, ensure_ascii=False, indent=4)
+        print(f"response[0]:{response[0]}")
         # return state
-        return {"hyde_embedding_chunks": []}
+        return {"hyde_embedding_chunks": response[0] if response else [], "hyde_doc": hyde_doc}  # []
+
+    def _step_1_create_embedding_hyde_doc(self, rewritten_query):
+        print("步骤1：基于大模型生成假设性文档")
+
+        # 1 客户端
+        ai_client = get_llm_client()
+
+        # 2 提示词
+        hyde_prompt = HYDE_PROMPT.format(rewritten_query=rewritten_query)
+
+        # 3 假设性回答
+        hyde_doc = ai_client.invoke(hyde_prompt).content
+
+
+        return hyde_doc
+
+    def _step_2_search_embedding_hyde(self, rewritten_query, hyde_doc, item_names):
+        print("步骤2：根据假设性文档进行向量搜索")
+        # 1 拼接上下文
+        embedding_context = rewritten_query + "" + hyde_doc
+
+        # 2 将上下文向量化
+        embeddings = generate_embeddings([embedding_context])
+
+        dense_vector = embeddings["dense"][0]
+        sparse_vector = embeddings["sparse"][0]
+
+        # 3 用"重写问题+假设性文档"搜索文本块
+        milvus_client = get_milvus_client()
+        collection_name = milvus_config.chunks_collection
+
+        reqs = create_hybrid_search_requests(
+            dense_vector=dense_vector,
+            sparse_vector=sparse_vector,
+            expr=f'item_name in {item_names}',
+        )
+        response = hybrid_search(
+            client=milvus_client,
+            collection_name=collection_name,
+            reqs=reqs,
+            limit=5,
+            output_fields=["chunk_id", "content", "item_name"]
+
+        )
+
+        # return state
+        return response
+
+
+if __name__ == "__main__":
+    node_search_embedding = NodeSearchEmbeddingHyde()
+    init_state = {
+        "item_names":["华为B3-211H显示器",],
+        "rewritten_query":"B3-211H显示器怎么使用呢"
+    }
+    process = node_search_embedding.process(state=init_state)
+    # print(json.dumps(process, ensure_ascii=False, indent=4))
--- processor/query_processor/nodes/e_processor/query_processor/nodes/e_node_rrf.py
+++ processor/query_processor/nodes/e_processor/query_processor/nodes/e_node_rrf.py
@@ -1,3 +1,6 @@
+import json
+from typing import List, Tuple
+
 from processor.query_processor.base import NodeBase
 from processor.query_processor.state import QueryGraphState
 from tool.logger import logger
@@ -22,4 +25,69 @@ class NodeRrf(NodeBase):
         # TODO
         logger.info(f"【{self.name}】节点逻辑")
 
+        # 1 参数处理
+        embedding_chunks = state.get("embedding_chunks")
+        hyde_embedding_chunks = state.get("hyde_embedding_chunks")
+
+        embedding_chunks_list = [doc.get("entity") for doc in embedding_chunks]
+        hyde_embedding_chunks_list = [doc.get("entity") for doc in hyde_embedding_chunks]
+
+        # 2 封装融合对象
+        rrf_input = [
+            (embedding_chunks_list, 1.0),
+            (hyde_embedding_chunks_list, 1.0)
+        ]
+
+        # 3 使用rrf算法公式对融合对象进行融合
+        rrf_merge_results = self._rrf_merge(rrf_input)
+
+        rrf_chunks = [doc for doc, _ in rrf_merge_results]
+        # 4 返回结果处理
+        state["rrf_chunks"] = rrf_chunks
+        print(f"rrf_chunks:{rrf_chunks}")
         return state
+
+    def _rrf_merge(self, rrf_input:List[Tuple], k:int=60, max_results: int=5):
+        print("向量搜索和假设性搜索的融合函数")
+        chunk_scores = {}
+        chunk_data = {}
+
+
+        # 循环处理融合数据
+        for rrf_input, weight in rrf_input:
+            for rank, doc in enumerate(rrf_input):
+                chunk_id = doc.get("chunk_id")
+                chunk_scores[chunk_id] = chunk_scores.get(chunk_id, 0.0) + weight / (k + rank)
+                chunk_data.setdefault(chunk_id, doc)
+
+        # 未排序结果
+        unsorted_results = [(chunk_data[cid], score) for cid, score in chunk_scores.items()]
+
+        # 排序结果
+        sorted_results = sorted(
+            unsorted_results,
+            key=lambda x: x[1],
+            reverse=True,
+        )
+
+        return sorted_results[:max_results]
+
+if __name__ == '__main__':
+
+    # 模拟两路检索结果
+    mock_state = {
+        "embedding_chunks": [
+            {"entity": {"chunk_id": "chunk_1", "content": "向量搜索结果#1"}},
+            {"entity": {"chunk_id": "chunk_2", "content": "向量搜索结果#2"}},
+            {"entity": {"chunk_id": "chunk_3", "content": "向量搜索结果#3"}},
+        ],
+        "hyde_embedding_chunks": [
+            {"entity": {"chunk_id": "chunk_1", "content": "HyDE搜索结果#1"}},
+            {"entity": {"chunk_id": "chunk_4", "content": "HyDE搜索结果#2"}},
+            {"entity": {"chunk_id": "chunk_2", "content": "HyDE搜索结果#3"}},
+        ]
+    }
+
+    node_rrf = NodeRrf()
+    result = node_rrf(mock_state)
+    logger.info(json.dumps(result, indent=4))
